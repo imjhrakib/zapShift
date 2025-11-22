c144dff7f7deb4cd1b1fd5f7dfffe82961ccc240
JHR <iamjhrakib@gmail.com>
2025-11-22 16:04:26 +0600

    added payment status

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+
+  const axiosSecure = useAxiosSecure();
+
+  console.log(sessionId);
+  useEffect(() => {
+    const res = axiosSecure
+      .patch(`/payment-success?session_id=${sessionId}`)
+      .then((res) => console.log(res.data));
+  }, [sessionId, axiosSecure]);
   return (
     <div>
       <h2>Payment Successfull</h2>
